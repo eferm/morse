8ff4ebc0e2647704f96f66a398f836081ba0aec1
Emanuel <gemore@gmail.com>
2008-11-20 15:07:14 +0100

    Updated and fixed. Accepted by tutor.

--- morse.c
+++ morse.c
@@ -9,18 +9,15 @@
 #include <stddef.h>
 
 #define MAX		29
-#define BUF		100
+#define BUF		300
+#define OFF		32
+#define A		65
+#define ADOT	197
+#define ADDOT	196
+#define ODOT	214
 
-int index_of(char);
+int index_of(unsigned char);
 const char * to_morse(char);
-
-/*
- * Two-dimensional array consisting of A-Ö and a-ö
- */
-const char letters[2][MAX] = {{65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 
-	77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 197, 196, 214},{97, 
-	98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 
-	113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 229, 228, 246}};
 
 /*
  * The morse code alphabet
@@ -33,14 +30,20 @@ const char *morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
 /*
  * index_of takes a character and returns the position of it in the alphabet
  */
-int index_of(char c)
+int index_of(unsigned char c)
 {
-	int i;
-	for (i = 0; letters[0][i] != c && letters[1][i] != c && i < MAX; i++)
-		;
-	if (i == MAX)
-		i = -1;
-	return i;
+	if (c > 64 && c < 91)
+		return (c - A);
+	else if (c > 96 && c < 123)
+		return (c - (A + OFF));
+	else if (c == ADOT || c == ADOT + OFF)
+		return (MAX - 3);
+	else if (c == ADDOT || c == ADDOT + OFF)
+		return (MAX - 2);
+	else if (c == ODOT || c == ODOT + OFF)
+		return (MAX - 1);
+	else
+		return -1;
 }
 
 /*
@@ -65,12 +68,12 @@ int main()
 	
 	fgets(s, BUF, stdin);
 	
-	while (s[i++] != EOF)
+	while (s[++i] != EOF)
 	{
 		if (s[i] == ' ')
 			putchar('\t');
 		else
-			printf("%s ", to_morse(s[i]));
+			printf(" %s", to_morse(s[i]));
 	}
 	putchar('\n');
 }
